40a2e2882865a902b4517e0ae63a2718bc3a35dc
ankit kumar <ant880423@gmail.com>
2024-03-09 01:09:10 +0530

    first commit2

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const HeroSection = () => {
   return (
     <section>
-        <h1 className="text-white"> Hello I'm Ankit</h1>
-        </section>
-  )
-}
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center">
+                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold"> 
+                Hello I'm Ankit
+                </h1>
+                <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores ducimus tenetur quam incidunt ex reprehenderit dicta. Voluptates, cum aliquid quae, nulla fugiat iusto repellendus corporis minima reiciendis explicabo, perspiciatis aut!
+                </p>
+            </div>
+            <div className="col-span-5"></div>
+        </div>
+    </section>
+  );
+};
 
 export default HeroSection
